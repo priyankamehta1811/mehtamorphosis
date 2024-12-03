79c7cdf9658422d428d9204d84047a50160e18fd
Priyanka Mehta <55352301+priyankamehta1811@users.noreply.github.com>
2024-12-03 11:44:39 +0530

    Update 6_📧_Contact.py

--- pages/6_📧_Contact.py
+++ pages/6_📧_Contact.py
@@ -2,6 +2,7 @@ import streamlit as st
 # Function to display the contact form
 
 st.header(":mailbox: Get In Touch With Me!")
+st.markdown("Feel free to contact with me:")
 
 contact_form = """
     <form action="https://formsubmit.co/priyanka.m@igib.in" method="POST">
@@ -16,12 +17,21 @@ contact_form = """
 st.markdown(contact_form, unsafe_allow_html=True)
 
 # Additional contact details
-st.write("")
-st.markdown("<div class='main-header'>Contact</div>", unsafe_allow_html=True)
-st.markdown("Feel free to reach out for collaborations or queries:")
-st.write("- **Email**: priyanka.m@igib.in")
-st.write("- **GitHub**: [priyankamehta1811](https://github.com/priyankamehta1811)")
-st.write("- **LinkedIn**: [priyankamehta1811](https://www.linkedin.com/in/priyankamehta1811/)")
+st.write("""
+
+         """)
+
+
+col1, col2, col3 = st.columns([1,1,1])  # Adjust column ratios as needed
+
+with col1:
+    st.link_button("My LinkedIn", "https://www.linkedin.com/in/priyankamehta1811/")
+        
+with col2:
+    st.link_button("My GitHub", "https://github.com/priyankamehta1811")
+
+with col3:       
+    st.link_button("My Twitter/X", "https://x.com/PriyankaMMehta")
 
 # Use local CSS File
 def local_css(file_name):
