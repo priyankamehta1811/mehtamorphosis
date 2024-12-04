1db2e3e0911a5331b908fc3f232360d74f1957fd
Priyanka Mehta <55352301+priyankamehta1811@users.noreply.github.com>
2024-12-05 01:25:50 +0530

    Update 3_📖_Publications.py

--- pages/3_📖_Publications.py
+++ pages/3_📖_Publications.py
@@ -1,9 +1,177 @@
 import streamlit as st
-st.header(":books: Publications!")
 
+# Add custom CSS
+custom_css = """
+<style>
+/* Import Google Font */
+@import url('https://fonts.googleapis.com/css2?family=Orbitron:wght@400;700&display=swap');
+
+/* Apply font globally */
+html, body, [class*="css"] {
+  font-family: 'Orbitron', sans-serif;
+  color: white;
+}
+
+.block-container
+{
+    padding-top: 4rem;
+    padding-bottom: 2rem;
+}
+
+/* Background */
+[data-testid="stSidebar"] {
+  background: linear-gradient(-45deg, #322C2B, #003C43, #D20062);
+  background-size: 400% 400%;
+  animation: gradient 15s ease infinite;
+  font-family: 'Orbitron', sans-serif;
+  margin: 0;
+  
+}
+
+/* Background */
+body,[data-testid="stAppViewContainer"],[data-testid="stHeader"] {
+  background: linear-gradient(-45deg, #322C2B, #003C43, #640D6B);
+  background-size: 400% 400%;
+  animation: gradient 15s ease infinite;
+  font-family: 'Orbitron', sans-serif;
+  margin: 0;
+}
+
+@keyframes gradient {
+    0% {
+      background-position: 0% 50%;
+    }
+    50% {
+      background-position: 100% 50%;
+    }
+    100% {
+      background-position: 0% 50%;
+    }
+  }
+
+/* Center content */
+.stApp {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  height: 100vh;
+  text-align: center;
+}
+
+/* Sidebar Navigation */
+[data-testid="stSidebarNav"] {
+  font-family: 'Orbitron', sans-serif !important;
+  color: #ff007f !important; /* Change font color */
+}
+
+/* Sidebar Navigation Links */
+[data-testid="stSidebarNav"] a {
+  color: #ff007f !important; /* Link color */
+  text-shadow: 0px 0px 3px #ff007f;
+  font-size: 16px;
+}
+
+/* Hover effect for navigation links */
+[data-testid="stSidebarNav"] a:hover {
+  color: #00ffe7 !important;
+  text-shadow: 0px 0px 5px #00ffe7;
+  transition: all 0.3s ease-in-out;
+}
+
+/* Background for sidebar */
+.css-1l02zno { 
+  background: linear-gradient(270deg, #003062, #005678, #ff2a6d, #24243e) !important;
+  color: white;
+}
+
+/* Scrollbar Styling */
+.sidebar ::-webkit-scrollbar {
+    width: 10px;
+}
+.sidebar ::-webkit-scrollbar-thumb {
+    background: #ff007f;
+    border-radius: 10px;
+}
+
+/* Scrollbar */
+::-webkit-scrollbar {
+  width: 10px;
+  
+}
+::-webkit-scrollbar-thumb {
+  background: #ff007f;
+  border-radius: 10px;
+}
+
+/* Headers */
+h1, h2, h3, h4, h5, h6 {
+  font-family: 'Orbitron', sans-serif ;
+  color: #00ffe7;
+  text-shadow: 0px 0px 5px #00ffe7, 0px 0px 20px #00ffe7;
+  text-align: center;
+}
+
+/* Paragraphs */
+p {
+  color: #f7f7f7;
+  text-shadow: 0px 0px 3px #ffffff;
+}
+
+/* Links */
+a {
+  color: #ff007f;
+  text-decoration: none;
+  text-shadow: 0px 0px 1px #ff007f;
+}
+a:hover {
+  color: #00ffe7;
+  text-shadow: 0px 0px 1px #00ffe7;
+}
+
+
+/* Sidebar Styling */
+.sidebar .sidebar-content {
+    background: linear-gradient(270deg, #003062, #005678, #ff2a6d);
+    padding: 20px;
+    color: white;
+    font-family: 'Orbitron', sans-serif;
+    box-shadow: 5px 0px 15px rgba(0, 0, 0, 0.3);
+    border-radius: 10px;
+}
+
+
+/* Inputs */
+input, textarea {
+  background-color: #24243e;
+  color: white;
+  border: 1px solid #00ffe7;
+  border-radius: 5px;
+  padding: 10px;
+}
+
+/* Add neon pink glow and drop shadow to images */
+img {
+  border: 0.1px solid #FFFFFF;
+  box-shadow: 0 0 5px #FFFFFF, 0 0 10px #FFFFFF, 0 0 20px #FFFFFF;
+  transition: all 0.3s ease;
+}
+
+img:hover {
+  box-shadow: 0 0 25px #00ffe7, 0 0 50px #00ffe7, 0 0 100px #00ffe7;
+}
+
+</style>
+"""
+# Inject the CSS into the Streamlit app
+st.markdown(custom_css, unsafe_allow_html=True)
+
+
+st.title(":books: Publications!")
 # Link to the publications page from the homepage
 st.link_button("Go to Google Scholar", "https://scholar.google.com/citations?user=W27to54AAAAJ&hl=en")
     
+
 # Inline CSS for the Streamlit app
 st.markdown("""
 <style>
@@ -84,7 +252,7 @@ st.markdown("""
 
 # Example of publications with links
 st.markdown("Here are some of my publications over the years:")
-st.markdown(""" **_2024_** """)
+st.markdown(""" #### **_2024_** """)
 st.markdown("1)  Suppressed transcript diversity and immune response in COVID-19 ICU patients: a longitudinal study. _Life Science Alliance_")
 st.markdown("[![1](https://www.life-science-alliance.org/content/lsa/7/1/e202302305/F1.large.jpg)](https://www.life-science-alliance.org/content/7/1/e202302305)")
 # Add more publications similarly as needed
@@ -98,7 +266,7 @@ st.markdown("4)  RNA editing in host lncRNAs as potential modulator in SARS-CoV-
 st.markdown("[![4](https://ars.els-cdn.com/content/image/1-s2.0-S258900422401068X-fx1.jpg)](https://www.sciencedirect.com/science/article/pii/S258900422401068X)")
 # Add more publications similarly as needed
 
-st.markdown(""" **_2023_** """)
+st.markdown(""" #### **_2023_** """)
 st.markdown("1)  Deficient Phagocytosis in Circulating Monocytes from Patients with COVID-19-Associated Mucormycosis _mBio_")
 st.markdown("[![1](https://journals.asm.org/cms/10.1128/mbio.00590-23/asset/dd932726-dfa1-4252-b709-7cf96a8e396b/assets/images/medium/mbio.00590-23-f001.gif)](https://doi.org/10.1128/mbio.00590-23)")
 # Add more publications similarly as needed
@@ -113,7 +281,7 @@ st.markdown("[![4](https://www.cell.com/cms/10.1016/j.isci.2023.108357/asset/549
 # Add more publications similarly as needed
 
 
-st.markdown(""" **_2022_** """)
+st.markdown(""" #### **_2022_** """)
 st.markdown("1)  Clinico-Genomic Analysis Reiterates Mild Symptoms Post-vaccination Breakthrough: Should We Focus on Low-Frequency Mutations? _Frontiers in Microbiology_")
 st.markdown("[![1](https://www.frontiersin.org/files/Articles/763169/fmicb-13-763169-HTML/image_m/fmicb-13-763169-g004.jpg)](https://doi.org/10.3389/fmicb.2022.763169)")
 # Add more publications similarly as needed
@@ -130,7 +298,7 @@ st.markdown("5)   Mutational dynamics across VOCs in International travellers an
 st.markdown("[![5](https://ars.els-cdn.com/content/image/1-s2.0-S0944501322001392-gr1.jpg)](https://doi.org/10.1016/j.micres.2022.127099)")
 # Add more publications similarly as needed
 
-st.markdown(""" **_2021_** """)
+st.markdown(""" #### **_2021_** """)
 st.markdown("1) Respiratory co-infections: modulators of SARS-CoV-2 patients’ clinical sub-phenotype. _Frontiers in Microbiology_")
 st.markdown("[![1](https://www.frontiersin.org/files/Articles/653399/fmicb-12-653399-HTML/image_m/fmicb-12-653399-g008.jpg)]( https://doi.org/10.3389/fmicb.2021.653399)")
 # Add more publications similarly as needed
@@ -145,12 +313,14 @@ st.markdown("[![4](https://www.frontiersin.org/files/Articles/753648/fgene-12-75
 
 
 
+
+
 # Footer
 st.markdown(
     """
     <hr>
     <p style='text-align: center;'>
-    © 2024 Priyanka Mehta | Ingen-hope Lab
+    © 2024 Priyanka Mehta | INGEN-HOPE Lab
     </p>
     """,
     unsafe_allow_html=True
